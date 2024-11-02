406558686659752e6702980847abcc16e4387945
Adam Stepanek <stepanek.ada@seznam.cz>
2024-11-02 19:42:47 +0100

    script with manual for url path generation

---
+++ src/pathGeneration/utils/jsonUtils.js
@@ -0,0 +1,19 @@
+function jsonStringifyWithSortedKeys(value) {
+    const replacer = (key, value) => value instanceof Object && !(value instanceof Array) ? Object.keys(value)
+        .sort()
+        .reduce((sorted, key) => {
+            sorted[key] = value[key];
+            return sorted;
+        }, {}) : value;
+    return JSON.stringify(value, replacer, 2);
+}
+
+// Remove any anti-CSRF prefixes (e.g., "for(;;);" or ")]}'") to make it valid JSON
+function removeAntiCsrfPrefixes(data) {
+    return data.replace(/^\)\]\}'\s*/, '').replace(/^for\s*\(\s*;\s*;\s*\)\s*;\s*/, '');
+}
+
+module.exports = {
+    jsonStringifyWithSortedKeys,
+    removeAntiCsrfPrefixes
+};
